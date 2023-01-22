d311e3b008f45a14f1de9b9733a334eb8b210e34
Fatality6 <83357001+Fatality6@users.noreply.github.com>
2023-01-22 15:57:50 +0300

    adding routes
    добавление роутинга

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Dashboard } from './pages/Dashboard/Dashboard'
 import { Login } from './pages/Login/Login'
+import { MyBoards } from './pages/MyBoards/MyBoards'
+import { Registration } from './pages/Registration/Registration'
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="registration" element={<Registration />}></Route>
+        <Route path="myboards" element={<MyBoards />}></Route>
+    </Routes>
   );
 }
 
